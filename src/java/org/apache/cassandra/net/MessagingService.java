3100a990a0d1ba5c24cc27757cdac9cc3e9180d5
Carter Casey <Carter.Casey@tufts.edu>
2016-04-10 22:30:41 -0600

    Reducing latency to 10 ms

--- src/java/org/apache/cassandra/net/MessagingService.java
+++ src/java/org/apache/cassandra/net/MessagingService.java
@@ -778,7 +778,7 @@ public final class MessagingService implements MessagingServiceMBean
 
 	if (message.verb == Verb.READ && ((int)Math.random() * 100) == 0) {
 		try {
-			Thread.sleep(25);
+			Thread.sleep(10);
 		} catch(InterruptedException e) {
 			Thread.currentThread().interrupt();
 		}
